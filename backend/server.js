6f77d1872fc4a135ae9b43cac550ae7e2541a279
Julia Grigorenko <jvgrigorenko@gmail.com>
2021-02-02 15:16:37 +0300

    connect to database in MongoDB

--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,14 @@ console.log(process.env.PORT);
 app.use(cors());
 app.use(express.json());
 
+const uri = "mongodb://localhost:27017/mern-exercise-tracker";
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+  );
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+  })
+
 app.get('/', async (req, res) => {
         res.send('hello');
     });
